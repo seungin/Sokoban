6fd7f135b07e2889d5ab225ef48b7add6ea8c090
seungin <seungin.cha@gmail.com>
2019-12-27 23:05:00 +0900

    Add scripts for item box and endpoint
    make item boxes detect triggering and endpoints rotate

---
+++ Assets/ItemBox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+	private Renderer renderer;
+
+	public Color originalColor;
+	public Color changedColor;
+
+	private void Start()
+	{
+		renderer = GetComponent<Renderer>();
+		originalColor = renderer.material.color;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "EndPoint")
+		{
+			renderer.material.color = changedColor;
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "EndPoint")
+		{
+			renderer.material.color = changedColor;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "EndPoint")
+		{
+			renderer.material.color = originalColor;
+		}
+	}
+}
